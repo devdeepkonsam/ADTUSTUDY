657df60698e8ce09125555fcb0b568fd1e019952
DEVDEEP SINGHA <67728391+devdeepkonsam@users.noreply.github.com>
2023-10-18 05:08:41 +0000

    problem15-20

---
+++ src/23BTCS112R/Problem15.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a ;
+    float b;
+    char c ;
+    double d ;
+    printf("int : %d \nfloat : %d \nchar : %d \ndouble : %d",sizeof(a),sizeof(b),sizeof(c),sizeof(d));
+    return 0;
+}
