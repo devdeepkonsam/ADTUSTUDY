723913f18a2ce3ae3d0ca6ca84ef2d080a20a792
DEVDEEP SINGHA <67728391+devdeepkonsam@users.noreply.github.com>
2023-10-26 08:33:44 +0000

    problem18

---
+++ src/23BTCS112R/Problem18.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, square; 
+    printf("Enter the Number :");
+    scanf("%d",&num);
+    square = num *num ;
+    printf("Square of %d is %d",num, square);
+    return 0;
+}
