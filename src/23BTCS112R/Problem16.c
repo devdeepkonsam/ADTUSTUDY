57e1b6e023d16b367c49d85839a19cc69b55c37b
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2023-11-14 10:30:17 +0530

    Update Problem16.c

--- src/23BTCS112R/Problem16.c
+++ src/23BTCS112R/Problem16.c
@@ -2,5 +2,17 @@
  
 int main()
 {
-    
+    int num1 = 2147483647;  //maximum value of int 
+    int num2 = -2147483649;  //minimum value of int 
+    printf("%d\n",num1);
+    printf("%d\n",num2);
+    int num3 = 2147483648;   
+    printf("%d\n",num3); //after adding 1 in maximun int value we get -2147483648
+
+
+    /*long keyword */
+    long int num =2147483649;
+    printf("%d\n",num);
+
+    return 0;
 }
